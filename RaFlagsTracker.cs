567041ea5a9a80cecaf818f6be63437d8b738558
Ramses Di Perna <ramses@sparklingsociety.net>
2023-03-17 14:35:26 +0100

    Added RaFlagsTracker to namespace

--- RaFlagsTracker.cs
+++ RaFlagsTracker.cs
@@ -1,140 +1,141 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
+using System.Collections.Generic;
 
-public class RaFlagsTracker : IDisposable
+namespace RaFlags
 {
-	public delegate void FlagHandler(object flag, RaFlagsTracker tracker);
-	public event FlagHandler FlagRegisteredEvent;
-	public event FlagHandler FlagUnregisteredEvent;
-	public event FlagHandler FlagsChangedEvent;
-
-	private HashSet<object> _flags = new HashSet<object>();
-
-	private FlagHandler _flagsChangedCallback = null;
-
-	public RaFlagsTracker(FlagHandler flagsChangedCallback = null)
+	public class RaFlagsTracker : IDisposable
 	{
-		_flagsChangedCallback = flagsChangedCallback;
-	}
+		public delegate void FlagHandler(object flag, RaFlagsTracker tracker);
+		public event FlagHandler FlagRegisteredEvent;
+		public event FlagHandler FlagUnregisteredEvent;
+		public event FlagHandler FlagsChangedEvent;
 
-	public IReadOnlyCollection<object> Flags => _flags;
+		private HashSet<object> _flags = new HashSet<object>();
 
-	public bool IsEmpty(params object[] flagsToExclude)
-	{
-		if(flagsToExclude != null && flagsToExclude.Length > 0)
+		private FlagHandler _flagsChangedCallback = null;
+
+		public RaFlagsTracker(FlagHandler flagsChangedCallback = null)
 		{
-			HashSet<object> temp = new HashSet<object>(_flags);
-			for(int i = 0; i < flagsToExclude.Length; i++)
+			_flagsChangedCallback = flagsChangedCallback;
+		}
+
+		public IReadOnlyCollection<object> Flags => _flags;
+
+		public bool IsEmpty(params object[] flagsToExclude)
+		{
+			if(flagsToExclude != null && flagsToExclude.Length > 0)
 			{
-				temp.Remove(flagsToExclude[i]);
+				HashSet<object> temp = new HashSet<object>(_flags);
+				for(int i = 0; i < flagsToExclude.Length; i++)
+				{
+					temp.Remove(flagsToExclude[i]);
+				}
+				return temp.Count == 0;
 			}
-			return temp.Count == 0;
-		}
-		else
-		{
-			return _flags.Count == 0;
-		}
-	}
-
-	public bool HasNone(object[] flags)
-	{
-		if(flags == null || flags.Length == 0)
-		{
-			return true;
-		}
-
-		foreach(object flag in flags)
-		{
-			if(HasFlag(flag))
+			else
 			{
-				return false;
+				return _flags.Count == 0;
 			}
 		}
 
-		return true;
-	}
-
-	public bool HasAll(object[] flags)
-	{
-		if(flags == null || flags.Length == 0)
+		public bool HasNone(object[] flags)
 		{
-			return true;
-		}
-
-		foreach(object flag in flags)
-		{
-			if(!HasFlag(flag))
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public bool HasAny(object[] flags)
-	{
-		if(flags == null || flags.Length == 0)
-		{
-			return true;
-		}
-
-		foreach(object flag in flags)
-		{
-			if(HasFlag(flag))
+			if(flags == null || flags.Length == 0)
 			{
 				return true;
 			}
-		}
-		return false;
-	}
 
-	public bool HasFlag(object flag)
-	{
-		return _flags.Contains(flag);
-	}
+			foreach(object flag in flags)
+			{
+				if(HasFlag(flag))
+				{
+					return false;
+				}
+			}
 
-	public bool Register(object flag)
-	{
-		if(_flags.Add(flag))
-		{
-			_flagsChangedCallback?.Invoke(flag, this);
-			FlagRegisteredEvent?.Invoke(flag, this);
-			FlagsChangedEvent?.Invoke(flag, this);
 			return true;
 		}
-		return false;
-	}
 
-	public bool Unregister(object flag)
-	{
-		if(_flags.Remove(flag))
+		public bool HasAll(object[] flags)
 		{
-			_flagsChangedCallback?.Invoke(flag, this);
-			FlagUnregisteredEvent?.Invoke(flag, this);
-			FlagsChangedEvent?.Invoke(flag, this);
+			if(flags == null || flags.Length == 0)
+			{
+				return true;
+			}
+
+			foreach(object flag in flags)
+			{
+				if(!HasFlag(flag))
+				{
+					return false;
+				}
+			}
 			return true;
 		}
-		return false;
-	}
 
-	public void Clear()
-	{
-		HashSet<object> temp = new HashSet<object>(_flags);
-		foreach(object flag in temp)
+		public bool HasAny(object[] flags)
 		{
-			Unregister(flag);
+			if(flags == null || flags.Length == 0)
+			{
+				return true;
+			}
+
+			foreach(object flag in flags)
+			{
+				if(HasFlag(flag))
+				{
+					return true;
+				}
+			}
+			return false;
 		}
-	}
 
-	public void Dispose()
-	{
-		FlagRegisteredEvent = null;
-		FlagUnregisteredEvent = null;
-		FlagsChangedEvent = null;
+		public bool HasFlag(object flag)
+		{
+			return _flags.Contains(flag);
+		}
 
-		_flagsChangedCallback = null;
-		_flags.Clear();
+		public bool Register(object flag)
+		{
+			if(_flags.Add(flag))
+			{
+				_flagsChangedCallback?.Invoke(flag, this);
+				FlagRegisteredEvent?.Invoke(flag, this);
+				FlagsChangedEvent?.Invoke(flag, this);
+				return true;
+			}
+			return false;
+		}
+
+		public bool Unregister(object flag)
+		{
+			if(_flags.Remove(flag))
+			{
+				_flagsChangedCallback?.Invoke(flag, this);
+				FlagUnregisteredEvent?.Invoke(flag, this);
+				FlagsChangedEvent?.Invoke(flag, this);
+				return true;
+			}
+			return false;
+		}
+
+		public void Clear()
+		{
+			HashSet<object> temp = new HashSet<object>(_flags);
+			foreach(object flag in temp)
+			{
+				Unregister(flag);
+			}
+		}
+
+		public void Dispose()
+		{
+			FlagRegisteredEvent = null;
+			FlagUnregisteredEvent = null;
+			FlagsChangedEvent = null;
+
+			_flagsChangedCallback = null;
+			_flags.Clear();
+		}
 	}
 }
